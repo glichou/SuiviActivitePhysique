724fde694e6873ba88523fa8256ca810156bcd99
Arzur Lichou <gregoire.lichou@lyc-parc-evry.ac-versailles.fr>
2021-03-09 21:14:23 +0100

    Ajout de quelques objets

--- SuiviActivitePhysique/source/Main.java
+++ SuiviActivitePhysique/source/Main.java
@@ -1,5 +1,15 @@
 
+/**
+ * Controleur de l'pplication de suivi d'activité physique.
+ * @author lichou
+ * @version 0.1
+ */
 public class Main {
+	
+	/**
+	 * Point d'entrée de l'application de suivi d'activité physique.
+	 * @param paramètres Les paramètres en entrée de l'application.
+	 */
 	public static void main(String[] parametres) {
 		System.out.println("Ouais ouais");
 	}
